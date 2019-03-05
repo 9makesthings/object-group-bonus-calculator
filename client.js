4c71cd90165554acf65fa3b71a6241d2982e34d8
Kingman Douglass <kreedd88@gmail.com>
2019-03-05 15:52:02 -0600

    first commit after initial completion

--- client.js
+++ client.js
@@ -33,6 +33,78 @@ const employees = [
 
 // YOU SHOULD NOT NEED TO CHANGE ANYTHING ABOVE THIS POINT
 
+$( document ).ready( function() {
+  $('#createButton').on( 'click', function(){
+    let addPerson = bonusCalculator( $( '#enterName' ).val());
+    let bonusList = $( '#bonus');
+    for (roster of employees){
+      bonusList.empty();
+      bonusList.append( '<li>' + BonusIssued.totalBonus + '</li>');
+    }
+  } )
+})
+
+ function bonusCalculator (roster) {
+   for( let i=0; i<employees.length; i++ ){
+      let person = employees[i];
+       if( roster === person.name){
+        // Checks reviewRating 
+        if( person.reviewRating <= 2){
+          bonusPercentage = 0;
+        }
+          else if( person.reviewRating == 3 ){
+          bonusPercentage = .04;
+           
+         }
+          else if( person.reviewRating == 4){
+           bonusPercentage = .06;
+          }
+            else if( person.reviewRating == 5){
+              bonusPercentage = .10;
+            }
+          // Checks tenure 
+          // Define variable to call length of string number
+          let employeeNum = person.employeeNumber.length;
+          if(employeeNum == 4 && person.reviewRating > 2){
+            bonusPercentage += .05;
+          }
+          // If income > $65,000
+          if( person.annualSalary > 65000 && person.reviewRating > 2){
+            bonusPercentage += -.01;
+          }
+          // No bonus can be above 13% 
+          if( bonusPercentage > .13 ){
+            bonusPercentage = .13;
+          }
+  
+          // Calculate totalComp
+          totalBonus = person.annualSalary * bonusPercentage;
+          totalComp = totalBonus + Number(person.annualSalary);
+
+      
+        // returns the new object if person exists
+        return new BonusIssued(roster, bonusPercentage, totalComp, totalBonus);
+      }
+   }
+   return false;
+}
+
+class BonusIssued {
+  constructor( name, bonusPercentage, totalComp, totalBonus){
+    this.name = name;
+    this.bonusPercentage = bonusPercentage;
+    this.totalComp = totalComp;
+    this.totalBonus = totalBonus
+  }
+}
+
+console.log(`is Atticus in array`, bonusCalculator('Atticus'));
+console.log(`is Atticus in array`, bonusCalculator('Jem'));
+console.log(`is Atticus in array`, bonusCalculator('Scout'));
+console.log(`is Atticus in array`, bonusCalculator('Robert'));
+console.log(`is Atticus in array`, bonusCalculator('Mayella'));
+
+
 // Take small steps! Don't write a for loop and two functions that do all of the calculations right away.
 // This problem is massive! Break the problem down. Use the debugger.
 // What is the fewest lines of code I can write and test to get just a little closer?
@@ -41,3 +113,5 @@ const employees = [
 // Ask questions when you don't.
 
 console.log( employees );
+
+
